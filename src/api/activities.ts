e5fdcae84441e43cd10a866651ee00abee7c85b3
abdallah-hatem <ahkortam@gmail.com>
2024-05-15 01:56:36 +0300

    pspspspsp

--- src/api/activities.ts
+++ src/api/activities.ts
@@ -1,17 +1,17 @@
 import { ApiBaseUrl, request } from './request';
 
 // Get activities
-export default (params?: any) => request<any>('get', `${ApiBaseUrl}activity`, params);
+export default (params?: any) => request<any>('get', `${ApiBaseUrl}/activity`, params);
 
 // Get activities by id
 export const GET_ACTIVITIES_BY_ID = (id: string, params?: any) =>
-  request<any>('get', `${ApiBaseUrl}activity/${id}`, params);
+  request<any>('get', `${ApiBaseUrl}/activity/${id}`, params);
 
 // Add category
-export const ADD_ACTIVITY = (data: any) => request<any>('post', `${ApiBaseUrl}activity`, data);
+export const ADD_ACTIVITY = (data: any) => request<any>('post', `${ApiBaseUrl}/activity`, data);
 
 // Delete category
-export const DELETE_ACTIVITY = (id: number) => request<any>('delete', `${ApiBaseUrl}activity/${id}`);
+export const DELETE_ACTIVITY = (id: number) => request<any>('delete', `${ApiBaseUrl}/activity/${id}`);
 
 // Update category
-export const UPDATE_ACTIVITY = (id: number, data: any) => request<any>('put', `${ApiBaseUrl}activity/${id}`, data);
+export const UPDATE_ACTIVITY = (id: number, data: any) => request<any>('put', `${ApiBaseUrl}/activity/${id}`, data);
